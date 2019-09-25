2b3e5744167cd34f381cc37313595579329f1edb
Bernardo Sze <bernardo.sze@gmail.com>
2019-09-25 15:51:48 -0400

    add Pug template engine

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,28 @@ const rootDir = require('../utils/path');
 
 const router = express.Router();
 
+const products = [];
+
+//@route   GET admin/add-product
+//@desc    add product
+//@access   PRIVATE
 router.get('/add-product', (req, res, next) => {
-	res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+	res.render('add-product', {
+		pageTitle: 'Add Product',
+		path: '/admin/add-product',
+		productsCSS: true,
+		formsCSS: true,
+		activeAddProduct: true
+	});
 });
 
+//@route   POST admin/add-product
+//@desc    add product
+//@access   PRIVATE
 router.post('/add-product', (req, res, next) => {
-	console.log(req.body);
+	products.push({ title: req.body.title });
 	res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,20 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+	const products = adminData.products;
+	res.render('shop', {
+		pageTitle: 'Shop',
+		path: '/',
+		products: products,
+		hasProducts: products.length > 0,
+		productsCSS: true,
+		activeShop: true
+	});
 });
 
 module.exports = router;
